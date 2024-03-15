ea34e3babb490e7ccd4ba747185905a927c02697
Student <Student@CLB3Unit26>
2024-03-15 14:26:31 +0800

    Gonzaga_Activity3

---
+++ src/gonzaga_activity3/Gonzaga_Activity3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gonzaga_activity3;
+
+/**
+ *
+ * @author Student
+ */
+public class Gonzaga_Activity3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        double Celsius = 10;
+        double Fahrenheit = (Celsius * 9/5) + 32;
+        
+        System.out.println("Temperature in Fahrenheit is: " + Fahrenheit + "F");
+    
+         }   
+}
